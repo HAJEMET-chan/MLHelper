8eb36fab7a4bd7e632033ab5b69721dbea8aa043
Gadzhimet Mursalov <vne4812@gmail.com>
2025-09-26 13:25:21 +0300

    create file system model

---
+++ mlhelper/ui/data_models/file_system.py
@@ -0,0 +1,12 @@
+import logging
+from PySide6.QtWidgets import QFileSystemModel
+
+logger = logging.getLogger(__name__)
+
+class FileSystemModel(QFileSystemModel):
+    def __init__(self):
+        super().__init__()
+
+        logger.debug("Initialized file system")
+
+        self.setRootPath("")
