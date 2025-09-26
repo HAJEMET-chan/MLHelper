e3a24554058a21b85b5322de3a7ea8b10e9c0d2c
Gadzhimet Mursalov <vne4812@gmail.com>
2025-09-26 13:27:42 +0300

    update main window to use explorer

--- mlhelper/ui/main_window.py
+++ mlhelper/ui/main_window.py
@@ -1,12 +1,19 @@
+import logging
 from PySide6.QtWidgets import QMainWindow
+from PySide6.QtCore import Qt
 
-from .custom_widgets.explorer import Explorer
+from .dock_widgets.explorer import Explorer
+
+logger = logging.getLogger(__name__)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        logger.info("Initialized main window")
+
         self.explorer: Explorer = Explorer()
+        self.set_dock_widgets()
 
     def set_dock_widgets(self):
-        self.addDockWidget(self.explorer)
+        self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, self.explorer)
